4ddfe439bdf0480c167918aee555034da639f7a1
Akulamax <32465485+Akulamax@users.noreply.github.com>
2017-10-22 00:45:27 +0300

    Update 21.10.2017CW.cpp

--- 21.10.2017CW.cpp
+++ 21.10.2017CW.cpp
@@ -1,28 +1,51 @@
+/*Дано: a,b,c - вещ.
+Напишите программу находящую решения уравнения ax^2+bx+c=0 */
 #include <iostream>
 #include <math.h>
 using namespace std;
-
 int main()
 {
-    double a,b,c,D,x,y,z;
+    double D, a, b, c, x, y, z;
     cin >> a >> b >> c;
-    D = (b*b- 4*a*c);
-    x = (-b-sqrt(D))/(2*a);
-    y = (-b+sqrt(D))/(2*a);
-    if ( (a==0)&& (b!=0) &&(c!=0) ){
-        z = (-c)/b;
-        cout << "radix" << z;
+    if (a != 0)
+    {
+        D = b*b - 4*a*c;
+        if (D >= 0)
+        {
+            x = (-b - sqrt(D)) / (2*a);
+            y = (-b + sqrt(D)) / (2*a);
+            cout << x << " " << y;
+        }
+        else
+        {
+            cout << "error (no any result)";
+        }
     }
-    if (a==0,b==0, c!=0) {
-        cout << "radix" << (-c);
+    else
+    {
+        if (b == 0)
+        {
+            if (c == 0)
+            {
+                cout << "radix-any";
+            }
+            else
+            {
+                cout << "error";
+            }
+        }
+        else
+        {
+            if (c == 0)
+            {
+                cout << "0";
+            }
+            else
+            {
+                z = -c / b;
+                cout << z;
+            }
+        }
     }
-    if (a==0,b==0,c==0){
-        cout << "radix" << " any";
-    }
-
-    if (D<0){
-    cout << "No any result";}
-    if (D>=0, a!=0,b!=0,c!=0){
-        cout << " first radix " << x << "second radix " << y;}
-        return 0;
+    return 0;
 }
