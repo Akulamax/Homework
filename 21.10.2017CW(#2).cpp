755fc862d2b09a56fed2efacab46c0ad5c2b833d
Akulamax <32465485+Akulamax@users.noreply.github.com>
2017-10-22 00:19:03 +0300

    Create 21.10.2017CW(#2).cpp

---
+++ 21.10.2017CW(#2).cpp
@@ -0,0 +1,22 @@
+/*Напишите программу, выписывающую среднее арифметическое всех введенных нечетных чисел. */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int s=0,n=0,a;
+    do
+    {
+        cin >>a;
+        if (a%2!=0)
+        {
+            s=s+a;
+            n=n+1;
+        }
+    }
+    while (a!=0);
+    double d;
+    d=(int)s/(int)n;
+    cout <<"result " << d;
+    return 0;
+}
