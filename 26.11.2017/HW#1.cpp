b7e573137a2dddc41170fb8d0657b62099740712
Akulamax <32465485+Akulamax@users.noreply.github.com>
2017-11-26 20:31:47 +0300

    Create HW#1.cpp

---
+++ 26.11.2017/HW#1.cpp
@@ -0,0 +1,46 @@
+/*Напишите программу, которая удаляет из массива все простые числа встречающиеся больше одного раза.*/
+#include <iostream>
+using namespace std;
+void delete_annoying_primes(int *A,int n){
+for (int i=0;i<n;i++)
+    cin>>A[i];
+}
+bool prostoe (int a){
+for (int i=2;i*i<=a;i++){
+if (a%i==0 && a!=2)
+    return false;
+}
+return true;
+}
+
+int main()
+{
+   int n;
+   cin>>n;
+   int *A=new int[n];
+   int *B=new int[n];
+   for (int k=0;k<n;k++){
+    B[k]=0;
+    }
+    delete_annoying_primes(A,n);
+   for (int i=0;i<n;i++){
+    if (!prostoe(A[i])||A[i]==1)
+    continue;
+    if(i<=n-2){
+    for ( int j=i+1;j<n;j++){
+    if (A[i]==A[j]){
+        B[i]=1;
+        B[j]=1;
+    }
+    }
+    }
+   }
+   for (int i=0; i<n;i++){
+   if (B[i]==0){
+ cout<<A[i]<<" ";
+   }
+   }
+delete []A;
+delete []B;
+    return 0;
+}
