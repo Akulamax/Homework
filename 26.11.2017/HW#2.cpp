8335d38d3eb40211f70e07b019b5fcaf0465e194
Akulamax <32465485+Akulamax@users.noreply.github.com>
2017-11-26 21:45:24 +0200

    Update HW#2.cpp

--- 26.11.2017/HW#2.cpp
+++ 26.11.2017/HW#2.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 void dig_trench(int** A,int n){
 for (int i=0;i<n;i++){
+A[i] = new int[n];
 for (int j=0;j<n;j++){
 A[i][j]=sqrt((i-j)*(i-j));}
 }
@@ -14,12 +15,6 @@ int main()
 {int n;
 cin>>n;
 int** Trench=new int *[n];
-for(int i = 0; i < n; i++) {
-Trench[i] = new int[n];
-for(int j = 0; j < n; j++) {
-Trench[i][j]=0;
-}
-}
 dig_trench(Trench,n);
 for (int i=0;i<n;i++){
 for (int j=0;j<n;j++){
@@ -27,6 +22,8 @@ cout<<Trench[i][j]<<" ";}
 for(int i=0;i<n;i=i+n)
 cout<<endl;
 }
+for (int i=0;i<n;i++)
+delete []Trench[i];
 delete []Trench;
 return 0;
 }
